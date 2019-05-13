cc820b7600c70b8424b27980114cceb009bb9645
小康 <123456@789.com>
2019-05-13 12:37:12 +0800

    Build

--- repo.js
+++ repo.js
@@ -173,7 +173,8 @@ function checkLocation () {
         `cd ${path.resolve(config.baseDir, site)}`,
         'git add -A',
         'git commit -m Build',
-        'git pull',
+        `git remote add origin gogs@${config.server}:${config.user}/${config.base}${site}.git`,
+        'git pull origin master',
         'git push'
       ]
     } else {
